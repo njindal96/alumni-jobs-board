138ac4c3446a5bcff9b68695c296b0b50646bda4
Naman Jindal <namanjindal150596@gmail.com>
2026-04-30 15:41:53 +0530

    chore: install vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,8 @@ export const metadata: Metadata = {
   description: "Discover exclusive opportunities tailored for our alumni network.",
 };
 
+import { Analytics } from "@vercel/analytics/react";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -27,7 +29,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
